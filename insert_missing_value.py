2514142a4c7192844304b8762bf809651953691c
jhmang1128 <67036244+jhmang1128@users.noreply.github.com>
2020-09-10 20:29:11 +0900

    Update insert_missing_value.py

--- insert_missing_value.py
+++ insert_missing_value.py
@@ -1,3 +1,9 @@
+'''
+작성일 : 2020-09-10
+작성자 : 맹주형
+코드 개요 : 궤적 데이터를 읽고 point(n-1)와 point(n)의 거리를 계산하여 거리가 특정 값 이상일때, 결측치를 삽입하는 코드
+'''
+
 import numpy as np
 import os
 import time
@@ -15,20 +21,52 @@ def processing_main():
     processing_data(interval_distance, load_path, save_path)
 
 def write_file(time, lat, lng, save_path, file_i):
+    '''
+    함수 개요 :
+        매개 변수로 받은 time, lat, lng 값을 add 모드로 한 줄씩 쓰기
+    매개변수 :
+        time = 시간 값
+        lat = 위도 값
+        lng = 경도 값
+        save_path = 파일이 저장될 경로
+        file_i = 저장될 파일의 이름
+    '''
     with open (save_path + file_i, 'a') as add_01:
         add_01.write(str(time) + ',' + lat + ',' + lng + '\n')
 
 def convert_to_stamp(time):
+    '''
+    함수 개요 :
+        datetime 데이터 형식의 시간 값을 timestamp 데이터 형식값으로 변환
+    매개변수 :
+        time = datetime 데이터 형식의 시간 값
+    '''
     time = datetime.strptime(time,'%Y%m%d%H%M%S')
     time_stamp = time.timestamp()
     return time_stamp
 
 def calculate_time(past_time, time):
+    '''
+    함수 개요 :
+        궤적의 point(n-1)와 point(n)의 시간 차를 계산하는 함수 (n은 1 부터 궤적의 point 갯 수까지 1씩 증가하는 값)
+    매개변수 :
+        past_time = point(n-1)의 시간 값
+        time = point(n)의 시간 값
+    '''
     time_01 = datetime.strptime(past_time,'%Y%m%d%H%M%S')
     time_02 = datetime.strptime(time,'%Y%m%d%H%M%S')
     return (time_02 - time_01).seconds
 
 def calculate_distance(p_lat, p_lng, lat, lng):
+    '''
+    함수 개요 :
+        궤적의 point(n-1)와 point(n)의 거리를 계산하는 함수 (n은 1 부터 궤적의 point 갯 수까지 1씩 증가하는 값)
+    매개변수 :
+        p_lat = point(n-1)의 위도 값
+        p_lng = point(n-1)의 경도 값
+        lat = point(n)의 위도 값
+        lng = point(n)의 경도 값
+    '''
     point = (float(lat), float(lng))
     p_point = (float(p_lat), float(p_lng))
     dis_meter = (haversine(p_point, point))*1000
@@ -36,6 +74,14 @@ def calculate_distance(p_lat, p_lng, lat, lng):
     
 
 def processing_data(interval_distance, load_path, save_path):
+    '''
+    함수 개요 :
+        궤적 데이터를 읽고 point(n-1)와 point(n)의 거리를 계산하여 거리가 특정 값 이상일때, 결측치를 삽입하는 코드
+    매개변수 :
+        interval_distance = 거리가 N미터 이상일 때 int(N/interval_distance)-1 갯 수만큼의 결측치를 삽입해주는 임계값
+        load_path = 궤적 데이터가 저장되어있는 경로
+        save_path = 결측치가 삽입된 궤적이 저장될 경로
+    '''
     version_list = os.listdir(load_path)
      
     missing_value = 'np.nan,np.nan'
